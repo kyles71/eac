5a003065cd1a3fbbabbd4ff3a10567d9be3b15f2
Kyle Schwab <kyleschwab71@gmail.com>
2026-04-05 10:35:38 -0400

    Add workflows for deployment

---
+++ deploy.php
@@ -0,0 +1,34 @@
+<?php
+namespace Deployer;
+
+require 'recipe/laravel.php';
+
+// Config
+
+set('repository', 'https://github.com/kyles71/eac.git');
+
+add('shared_files', ['.env']);
+add('shared_dirs', ['storage']);
+add('writable_dirs', []);
+
+// Hosts
+host(getenv('DEPLOY_HOST'))
+    ->setLabels([
+        'env' => 'dev',
+    ])
+    ->set('remote_user', getenv('DEPLOY_USER'))
+    ->set('sudo_password', getenv('DEPLOY_PASSWORD'))
+    ->set('deploy_path', '/var/www/html/eac-test');
+
+// Tasks
+
+desc('Install & build npm packages');
+
+task('npm:build', function () {
+    run('cd {{release_path}} && npm ci && npm run build');
+});
+
+// Hooks
+
+after('artisan:migrate', 'npm:build');
+after('deploy:failed', 'deploy:unlock');
