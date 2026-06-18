153243162ffafe20302c45323be590f0f50885d1
Kyle Schwab <kyleschwab71@gmail.com>
2026-06-18 11:17:20 -0400

    Update migration

--- database/migrations/2026_06_18_000002_create_product_early_access_window_user_table.php
+++ database/migrations/2026_06_18_000002_create_product_early_access_window_user_table.php
@@ -11,11 +11,19 @@ return new class() extends Migration
     public function up(): void
     {
         Schema::create('product_early_access_window_user', function (Blueprint $table) {
-            $table->foreignId('product_early_access_window_id')->constrained()->cascadeOnDelete();
-            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('product_early_access_window_id');
+            $table->foreignId('user_id');
             $table->timestamps();
 
-            $table->primary(['product_early_access_window_id', 'user_id']);
+            $table->primary(['product_early_access_window_id', 'user_id'], 'peaw_user_window_user_primary');
+            $table->foreign('product_early_access_window_id', 'peaw_user_window_id_fk')
+                ->references('id')
+                ->on('product_early_access_windows')
+                ->cascadeOnDelete();
+            $table->foreign('user_id', 'peaw_user_user_id_fk')
+                ->references('id')
+                ->on('users')
+                ->cascadeOnDelete();
         });
     }
 
